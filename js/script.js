3495774b4524259c5f50d63f98600eba40c11d7f
Cole Tomlinson <cmt218@lehigh.edu>
2018-05-09 17:46:24 -0400

    ideas from research and spinning box demo

--- js/script.js
+++ js/script.js
@@ -1,65 +1,24 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-var renderer = new THREE.WebGLRenderer();
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor (0xffffff, 1);
+var scene = new THREE.Scene(); 
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
+var renderer = new THREE.WebGLRenderer(); 
+renderer.setSize( window.innerWidth, window.innerHeight ); 
 document.body.appendChild( renderer.domElement );
 
-var light = new THREE.PointLight( 0xffffff, 1, 100 );
-light.position.set( 1, 1, 1 );
-scene.add( light );
-
 var controls = new THREE.OrbitControls( camera );
-var spinner = null;
-var numspinner = 100;
-var spinners = [];
-
-
-var objectLoader = new THREE.ObjectLoader();
-objectLoader.load("assets/FidgetSpinner.json", function ( obj ) {
-spinner = obj;
-spinner.rotation.x += 0.5;
-for ( var i = 0; i < numspinner; i ++ ) {
-	var mesh = obj.clone();
-	mesh.position.x = 400 * ( 0.5 - Math.random() );
-	mesh.position.y = 400 * ( 0.5 - Math.random() ) + 25;
-	mesh.position.z = 400 * ( 0.5 - Math.random() );
-	mesh.rotation.y = 3.14 * ( 0.5 - Math.random() );
-	mesh.rotation.x = 3.14 * ( 0.5 - Math.random() );
-	mesh.matrixAutoUpdate = false;
-	mesh.updateMatrix();
-	spinners[i] = mesh;
-	scene.add( mesh );
-}
-
-scene.add( obj );
-} );
-
+var geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); 
+var cube = new THREE.Mesh( geometry, material ); 
+scene.add( cube ); 
 camera.position.z = 5;
-controls.autoRotate = true;
-controls.rotateSpeed = 2.0;
 
-function animate() {
-	requestAnimationFrame( animate );
-	if(spinner){
-		spinner.rotation.y+=spinspeed;
-	}
-	
+function animate() { 
+	requestAnimationFrame( animate ); 
+	cube.rotation.x += 0.1; 
+	cube.rotation.y += 0.1;
+
 	controls.update();
-	renderer.render( scene, camera );
-}
+
+	renderer.render( scene, camera ); 
+} 
 
 animate();
-
-var spinspeed = 0.00;
-
-function increasespeed(){
-	spinspeed += 0.05;
-}
-
-function decreasespeed(){
-	spinspeed -= 0.05;
-}
