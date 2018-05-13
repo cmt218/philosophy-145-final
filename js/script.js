beff3199bd9d6adc6ccdfdec8f48c96ce5278cf7
Cole Tomlinson <cmt218@lehigh.edu>
2018-05-13 13:41:13 -0400

    before trying experimental text stuff

--- js/script.js
+++ js/script.js
@@ -1,24 +1,58 @@
 var scene = new THREE.Scene(); 
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
 var renderer = new THREE.WebGLRenderer(); 
-renderer.setSize( window.innerWidth, window.innerHeight ); 
-document.body.appendChild( renderer.domElement );
+renderer.setSize( window.innerWidth/1.15, window.innerHeight/1.15 ); 
+renderer.setClearColor (0xffffff, 1);
+var container = document.getElementById('container');
+container.appendChild(renderer.domElement);
+var info = document.getElementById('info');
 
 var controls = new THREE.OrbitControls( camera );
-var geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); 
-var cube = new THREE.Mesh( geometry, material ); 
-scene.add( cube ); 
-camera.position.z = 5;
+var objectLoader = new THREE.ObjectLoader();
 
+
+
+var brain = null;
+
+welcome();
+animate();
+
+camera.position.z = 1;
+
+// Animation function
 function animate() { 
 	requestAnimationFrame( animate ); 
-	cube.rotation.x += 0.1; 
-	cube.rotation.y += 0.1;
 
+	if(brain) {
+		brain.rotation.y += 0.02;
+	}
+	
 	controls.update();
 
 	renderer.render( scene, camera ); 
 } 
 
-animate();
+
+
+// Displays spinning brain intro
+function welcome() {
+	
+	// Colors
+	var color = 0xa8a3a5;
+	var htmlcolor = "#a8a3a5";
+
+	// Setup BG color and light
+	renderer.setClearColor (color, 1);
+	container.style.backgroundColor = htmlcolor;
+	var light = new THREE.PointLight( 0xce4886, 1, 100 );
+	light.position.set( 0, 0, 1 );
+	scene.add( light );
+
+	// Load in brain model
+	objectLoader.load("assets/brain.json", function ( obj ) {
+		brain = obj;
+		brain.scale.set(3,3,3);
+		brain.position.y = -0.25;
+		scene.add(brain);
+	});
+}
